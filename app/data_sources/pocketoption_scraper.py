a4874a2855972c0c8e20630ef2d0d249b1714d1d
Bilenss <bileenss@gmail.com>
2025-09-07 23:33:26 +0500

    Update pocketoption_scraper.py

--- app/data_sources/pocketoption_scraper.py
+++ app/data_sources/pocketoption_scraper.py
@@ -95,7 +95,7 @@ async def generate_fast_data(symbol: str, timeframe: str, otc: bool) -> pd.DataF
     
     # Создаем временной индекс
     freq_map = {
-        '30s': '30S', '1m': '1min', '2m': '2min', '3m': '3min',
+        '30s': '30s', '1m': '1min', '2m': '2min', '3m': '3min',
         '5m': '5min', '10m': '10min', '15m': '15min', 
         '30m': '30min', '1h': '1H'
     }
