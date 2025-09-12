4ced98110d658260acc993e0ada3142621bbdb79
Bilenss <bileenss@gmail.com>
2025-09-12 11:25:03 +0500

    Delete telegram-forecast-bot-main directory

--- telegram-forecast-bot-main/app/utils/logging.py
+++
@@ -1,8 +0,0 @@
-import sys
-from loguru import logger
-
-def setup(level: str = "INFO"):
-    logger.remove()
-    logger.add(sys.stdout, level=level, backtrace=False, diagnose=False,
-               format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{message}</cyan>")
-    return logger
